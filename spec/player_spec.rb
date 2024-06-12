b9d56a7fecf919d51626e115361ff2c4d76c4fe9
Derek Chavez <frrenose17@yahoo.com>
2024-06-12 09:56:59 -0600

    feat: ad attributes to player and has lost method

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,35 +1,25 @@
+require 'rspec'
 require './lib/player'
 require './lib/deck'
 require './lib/card'
 
-Rspec.describe Player do
+RSpec.describe Player do
   it "exists" do
-    player = Player.new("Clarisa", deck)
+    player = Player.new('Clarisa', 'deck')
 
     expect(player).to be_an_instance_of(Player)
   end
 
   it "has readable attributes" do
-    player = Player.new("Clarisa", deck)
+    player = Player.new('Clarisa', 'deck')
 
-    expect(player.name).to eq("Clarisa")
-    expect(player.deck).to eq(deck)
+    expect(player.name).to eq('Clarisa')
+    expect(player.deck).to eq('deck')
   end
 
   it "can determine if player has lost" do
-    player = Player.new("Clarisa", deck)
-    card1 = Card.new(:diamond, 'Queen', 12)
-    card2 = Card.new(:spade, '3', 3)
-    card3 = Card.new(:heart, 'Ace', 14)
-    deck.cards << card1
-    deck.cards << card2
-    deck.cards << card3
-
-    expect(player.has_lost?).to eq(false)
-
-    deck.remove_card
-    deck.remove_card
-    deck.remove_card
+    deck = Deck.new
+    player = Player.new('Clarisa', deck)
 
     expect(player.has_lost?).to eq(true)
   end
